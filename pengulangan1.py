2a1dac3fcbb2f9f03121c5eb510c9e0242cf9a52
tysar budirianto <43081491+sirtysar@users.noreply.github.com>
2019-03-22 11:18:45 +0700

    Create pengulangan1.py

---
+++ pengulangan1.py
@@ -0,0 +1,11 @@
+# contoh_1
+for i in [1, 2, 3, 4, 5]:
+    print "Ini pengulangan ke - ", i
+
+# contoh_2
+for i in ["Rawon", "Nasi Kuning", "Soto Madura", "Kupat Tahu", "Kerak Telor", "Rendang Batoko", "Pempek Selam", "Ayam Betutu"]:
+    print i, " adalah masakan khas nusantara ..."
+
+# contoh_3
+for i in "abcde":
+    print i, " adalah alfabet"
