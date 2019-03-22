33c13da018b8e11c5b6b1d8091c77b59a7217c7a
tysar budirianto <43081491+sirtysar@users.noreply.github.com>
2019-03-22 15:26:46 +0700

    Create strukdat1.py

---
+++ strukdat1.py
@@ -0,0 +1,22 @@
+# cara mendefinisikan list
+sebuah_list = ['Zorin OS', 'Ubuntu', 'FreeBSD', 'NetBSD', 'OpenBSD', 'Backtrack', 'Fedora', 'Slackware']
+
+# cara mendefinisikan tuple
+sebuah_tuple = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+# cara mendefinisikan dictionary
+sebuah_dictionary = {'nama':'Wiro Sableng',
+                    'prodi':'ilmu komputer',
+                    'email':'wirosableng@localhost',
+                    'website':'http://www.sitampanggarang.com'
+                    }
+
+
+print "mengakses salah satu elemen : "
+print sebuah_list[5]
+print sebuah_tuple[8]
+print sebuah_dictionary['website']
+print "\n"
+print "mengakses beberapa elemen : "
+print sebuah_list[2:5]
+print sebuah_tuple[3:6]
